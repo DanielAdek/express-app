fb58954152f5b02ebe3ff9b5b361602e8f63f922
Akdan <akdan1111111@gmail.com>
2018-04-15 22:57:24 +0100

    create dummydb

---
+++ database/userdb.js
@@ -0,0 +1,14 @@
+const user = [
+    {
+        id: 1,
+        name: 'daniel',
+        business: 'andela'
+    },
+    {
+        id: 2,
+        name: 'mikee',
+        business: 'film'
+    }
+]
+
+module.exports = user;
